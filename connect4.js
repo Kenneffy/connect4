cfc33ddb0ac6abfdc6af9829a48819a4047d823a
Kenneth Yee <Kenneth_yee2@yahoo.com>
2015-06-24 16:10:38 -0400

    finished event handlers and set turn

--- connect4.js
+++ connect4.js
@@ -1,1 +1,17 @@
-console.log("loaded bro")
+$( document ).ready(function() {
+	console.log("loaded bro")
+
+	var turn = 0;
+
+	$('#turn').text(turn);
+
+	$('#columnOne, #columnTwo, #columnThree, #columnFour, #columnFive, #columnSix, #columnSeven').on('click', function(){
+		if (turn == 0){
+			turn = 1;
+			$('#turn').text(turn);
+		}else if(turn == 1){
+			turn = 0;
+			$('#turn').text(turn);
+		}
+	});
+});
